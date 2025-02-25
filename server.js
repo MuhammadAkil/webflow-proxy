c0d41d34ce35b8b63352b6077f1a8e70b2e1ae73
Akil <akil.siddique@postex.pk>
2025-02-25 12:40:19 +0500

    fetch items from webflow

--- server.js
+++ server.js
@@ -6,16 +6,24 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// CORS ko allow karne ke liye middleware use kar rahe hain
 app.use(cors()); 
 app.use(express.json());
 
-// Webflow API se blogs fetch karne ka endpoint
+const corsOptions = {
+  origin: "http://localhost:4200", // Allow frontend requests
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+
 app.get("/api/webflow/blogs", async (req, res) => {
   try {
     const response = await axios.get(
     //   "https://api.webflow.com/v2/collections/67b479a0870ee7b88b5eaf55/items",
-      "https://api.webflow.com/v2/sites/67b4791569e93db872775575/collections",
+        "https://api.webflow.com/v2/sites/67b4791569e93db872775575/collections",
+      
       {
         headers: {
           Authorization: `Bearer ${process.env.WEBFLOW_API_KEY}`,
